13a430af9308437557cadae41fcc14575e979dc0
Alejandro Alonso López <aalonsopersonal@gmail.com>
2023-09-11 14:01:18 +0200

    First approach using "any" type on function declarations

--- src/controllers/preprocessing.ts
+++ src/controllers/preprocessing.ts
@@ -1,23 +1,75 @@
 import { Response, Request } from "express";
 import { Logger } from "../utils/Logger";
+import { get } from "http";
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 const getLeaflet = (epi: any) => {
   let leafletSectionList = epi['entry'][0]['resource']['section'][0]['section']
   return leafletSectionList
 }
 
-const addSemmanticAnnotation = (leafletSectionList: any[]) => {
+const getSnomedCodes = async () => {
+  const snomedCodes = await fetch('https://raw.githubusercontent.com/HL7/UTG/master/ig.json').then((response) => {
+    return response.json()
+  }).then((data) => {
+    return data['expansion']['contains']
+  })
+  return snomedCodes
+}
+
+function annotationProcess(divString: string, code: object) {
+  let dom = new JSDOM(divString);
+  let document = dom.window.document;
+  let leaflet = document.querySelectorAll('div');
+  recursiveTreeWalker(leaflet, code, document);
+
+  return document.documentElement.outerHTML;
+}
+
+function recursiveTreeWalker(nodeList: any, code: any, document: any) {
+  for (let node of nodeList) {
+      if (node.childNodes.length > 0) {
+          if (node.childNodes.length == 1) {
+              if (node.childNodes[0].nodeName == '#text') {
+                  if (node.childNodes[0].textContent.includes(code["en"])) {
+                      const span = document.createElement('span');
+                      span.className = code["ID"];
+                      span.textContent = node.childNodes[0].textContent;
+                      node.childNodes[0].textContent = '';
+                      node.appendChild(span);
+                  }
+                  else {
+                      recursiveTreeWalker(node.childNodes, code, document);
+                  }
+              } else {
+                  recursiveTreeWalker(node.childNodes, code, document);
+              }
+          } else {
+              recursiveTreeWalker(node.childNodes, code, document);
+          }
+      } else {
+          if (node.textContent.includes(code["en"])) {
+              const span = document.createElement('span');
+              span.className = code["ID"];
+              span.textContent = node.childNodes[0].textContent;
+              node.childNodes[0].textContent = '';
+              node.appendChild(span);
+          }
+      }
+  }
+}
+
+const addSemmanticAnnotation = (leafletSectionList: any[], snomedCodes: any[]) => {
   let preprocessedSections: any = []
-  leafletSectionList.forEach(section => {
-    //Preprocessing goes in here
-    // ADD TEST PREPROCESS
-    console.log("Adding <samplePreprocessTag> tag");
-    section['text']['div'] = `<samplePreprocessTag><samplePreprocessTag3>${section['text']['div']}</samplePreprocessTag></samplePreprocessTag3>`
-
-
-
-
-    preprocessedSections.push(section)
+  leafletSectionList.forEach((section) => {
+    const divString = section['text']['div']
+    snomedCodes.forEach((code) => {
+      if (divString.includes(code['en'])) {
+        section['text']['div'] = annotationProcess(divString, code)
+      }
+    })
   })
   return preprocessedSections
 }
@@ -27,6 +79,13 @@ export const preprocess = async (req: Request, res: Response) => {
   console.log(`Received ePI with Length: ${JSON.stringify(epi).length}`);
   Logger.logInfo('preprocessing.ts', 'preprocess', `queried /preprocess function with epi ID: ${JSON.stringify(epi['id'])}`)
   let leafletSectionList
+  let snomedCodes
+  try {
+    snomedCodes = await getSnomedCodes()
+  } catch (error) {
+    res.status(500).send('Failed getting Snomed Codes')
+    return
+  }
   try {
     leafletSectionList = getLeaflet(epi)
   } catch (error) {
@@ -35,12 +94,17 @@ export const preprocess = async (req: Request, res: Response) => {
   }
   let annotatedSectionList
   try {
-    annotatedSectionList = addSemmanticAnnotation(leafletSectionList)
+    annotatedSectionList = addSemmanticAnnotation(leafletSectionList, snomedCodes)
   } catch (error) {
     res.status(500).send('Preprocessor error')
     return
   }
   epi['entry'][0]['resource']['section'][0]['section'] = annotatedSectionList
+  epi["entry"][0]["resource"]["category"][0]["coding"][0] = {
+    "system": epi["entry"][0]["resource"]["category"][0]["coding"][0]["system"],
+    "code": "P",
+    "display": "Preprocessed"
+  }
   console.log(`Returning ePI with Length: ${JSON.stringify(epi).length}`);
   res.status(200).send(epi);
   return
